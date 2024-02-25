1e7f2f06606026ba1cc68c20d05b02c43df82f04
Akash Mulye <akashmulye97@gmail.com>
2024-02-25 15:22:45 -0500

    mobiel styling markets

--- src/Components/MarketsSectors/index.jsx
+++ src/Components/MarketsSectors/index.jsx
@@ -8,6 +8,7 @@ export const MarketsSectors = () => {
     const [securityData, setSecurityData] = useState([]);
     const [miscData, setMiscData] = useState([]);
     const [activeTab, setActiveTab] = useState('security');
+    const [width] = useState(window.innerWidth);
 
     const fetchData = async () => {
         try {
@@ -39,52 +40,81 @@ export const MarketsSectors = () => {
                 <div className='section-text'>
                     {title && (<h1>{title}</h1>)}
                 </div>
-                <div className='table'>
-                    {securityData && miscData ? (
-                        <div className='tabs'>
-                            <h3 
-                                className={`tab ${activeTab === 'security' ? 'active' : ''}`}
-                                onClick={(e) => handleTabClick(e)} 
-                                name='security'
-                            >
-                                    {securityData.tabTitle}
-                            </h3>
-                            <h3 
-                                className={`tab ${activeTab === 'misc' ? 'active' : ''}`}
-                                onClick={(e) => handleTabClick(e)} 
-                                name='misc'
-                            >
-                                {miscData.tabTitle}
-                            </h3>
+                {width > 768 ? (
+                    <div className='table'>
+                        {securityData && miscData ? (
+                            <div className='tabs'>
+                                <h3 
+                                    className={`tab ${activeTab === 'security' ? 'active' : ''}`}
+                                    onClick={(e) => handleTabClick(e)} 
+                                    name='security'
+                                >
+                                        {securityData.tabTitle}
+                                </h3>
+                                <h3 
+                                    className={`tab ${activeTab === 'misc' ? 'active' : ''}`}
+                                    onClick={(e) => handleTabClick(e)} 
+                                    name='misc'
+                                >
+                                    {miscData.tabTitle}
+                                </h3>
+                            </div>
+                        ) : (
+                            null
+                        )}
+                        <div className='content'>
+                            <div className={`list security-content ${activeTab === 'security' ? 'active' : ''}`}>
+                                {securityData.items ? (
+                                    <ul>
+                                        {securityData.items.map((item, index) => {
+                                            return <li key={index}>{item}</li>
+                                        })}
+                                    </ul>
+                                ) : (
+                                    <h3>Nothing to see here</h3>
+                                )}
+                            </div>
+                            <div className={`list misc-content ${activeTab === 'misc' ? 'active' : ''}`}>
+                                {miscData.items ? (
+                                    <ul>
+                                        {miscData.items.map((item, index) => {
+                                            return <li key={index}>{item}</li>
+                                        })}
+                                    </ul>
+                                ) : (
+                                    <h3>Nothing to see here</h3>
+                                )}
+                            </div>
                         </div>
-                    ) : (
-                        null
-                    )}
-                    <div className='content'>
-                        <div className={`list security-content ${activeTab === 'security' ? 'active' : ''}`}>
-                            {securityData.items ? (
-                                <ul>
-                                    {securityData.items.map((item, index) => {
-                                        return <li key={index}>{item}</li>
-                                    })}
-                                </ul>
-                            ) : (
-                                <h3>Nothing to see here</h3>
+                    </div>
+                ) : (
+                    <div className='mobile-table'>
+                        <div className='container'>
+                            {securityData && securityData.items && (
+                                <>
+                                    <h4>{securityData.tabTitle}</h4>
+                                    <ul>
+                                        {securityData.items.map((item, index) => {
+                                            return <li key={index}>{item}</li>
+                                        })}
+                                    </ul>
+                                </>
                             )}
                         </div>
-                        <div className={`list misc-content ${activeTab === 'misc' ? 'active' : ''}`}>
-                            {miscData.items ? (
-                                <ul>
-                                    {miscData.items.map((item, index) => {
-                                        return <li key={index}>{item}</li>
-                                    })}
-                                </ul>
-                            ) : (
-                                <h3>Nothing to see here</h3>
+                        <div className='container'>
+                            {miscData && miscData.items && (
+                                <>
+                                    <h4>{miscData.tabTitle}</h4>
+                                    <ul>
+                                        {miscData.items.map((item, index) => {
+                                            return <li key={index}>{item}</li>
+                                        })}
+                                    </ul>
+                                </>
                             )}
                         </div>
                     </div>
-                </div>
+                )}
             </section>
         </>
     )
